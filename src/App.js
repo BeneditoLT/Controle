cbc7e54860d0d0b3856efbf7c17af68eb05b8952
Lucas <benedito.lucas@gmail.com>
2023-05-04 02:02:16 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './componet/Pages/Home';
+import Projetos from './componet/Pages/Projetos';
+import Empresa from './componet/Pages/Empresa';
+import Contato from './componet/Pages/Contato';
+import Container from './componet/layout/Container';
+import Navbar from './componet/layout/Navbar';
+import Footer from './componet/layout/Footer';
+
+
+function App() {
+  return (
+    <Router>
+    
+    <Navbar/>
+    <Container customClass="min-height">
+      <Routes>
+        
+        <Route path="/"       element={<Home/>}></Route>
+        <Route path="/Projetos"element={<Projetos/>}></Route>
+        <Route path="Empresa" element={<Empresa/>}></Route>
+        <Route path="/Contato"element={<Contato/>}></Route>
+        
+      </Routes>
+      </Container>
+      <Footer/>
+    </Router>
+    );
+}
+
+export default App;
---
+++ src/componet/layout/Navbar.js
@@ -0,0 +1,27 @@
+import {Link} from 'react-router-dom'
+import Container from './Container'
+import styles from './Navbar.module.css'
+
+import logo from '../../img/costs_logo.png'
+
+function Navbar() {
+
+    return(
+        
+        <nav className={styles.navbar}>
+            <Container customClass="min-height">
+            <ul className={styles.list}>  
+            <li className={styles.logo}> <Link to="/"> <img src={logo} alt="Cost"/></Link></li>
+            <li className={styles.item}><Link to="/">Home</Link></li>
+            <li className={styles.item}><Link to="/Projetos">Projetos</Link></li>
+            <li className={styles.item}><Link to="/Empresa">Empresa</Link></li>
+            <li className={styles.item}><Link to="/Contato">Contato</Link> </li>
+            </ul>
+
+            </Container>
+
+        </nav>
+    )
+
+}
+export default Navbar
