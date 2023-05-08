f1d3bf3c10ef8322ee009222de8d73c5ef74614e
Benedito Lucas de Toledo <46333024+BeneditoLT@users.noreply.github.com>
2023-05-08 00:26:14 -0300

    Add files via upload

--- src/componet/projeto/ProjetoFrom.js
+++ src/componet/projeto/ProjetoFrom.js
@@ -1,13 +1,29 @@
+import { useEffect, useState } from "react"
 import Input from "../form/Input"
 import Select from "../form/Select"
 import SubmitButton from "../form/SubmitButton"
 
 function ProjetoFrom({btnText}){
+    const [categories, setCategories] =useState([])
+
+    useEffect(() =>{
+        fetch('http://localhost:5000/categories', {
+        method: "GET",
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
     return(
         <from>
             <Input type="text" text="Nome do Projeto" name="name" placeholder="Incira o nome do Projeto"/>
             <Input type="number" text="Orçamento do Projeto" name="budget" placeholder="Incira o orçamento total"/>
-            <Select name="category_id" text="Selecione uma categoria"/>
+            <Select name="category_id" text="Selecione uma categoria" options={categories}/>
             <SubmitButton text={btnText}/>
             
         </from>
